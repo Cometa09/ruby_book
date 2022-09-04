f66f16a040ab8fb60a4c67da1b52057ba5eb3a25
Cometa09 <sokolovalex09@gmail.com>
2022-09-04 18:27:14 +0300

    Update Module condition, add varible instance, end part 13

--- 13 tests/test/test_list_with_commas.rb
+++ 13 tests/test/test_list_with_commas.rb
@@ -3,24 +3,32 @@ require 'list_with_commas'
 
 class TestListWithCommas < Minitest::Test
 
+    def setup
+        @list = ListWithCommas.new
+    end
+
+=begin   
+    Убрали дублирование кода, испульзуем перемнную экземпляра @list
+    Метод setup выполняется всегда при запуске тестирования
+    Метод teardown выыполняется всегда после выполнения каждого теста, 
+    например сделать зачистку
+=end
+
     def test_it_prints_one_word_alone
-        list = ListWithCommas.new
-        list.items = ['apple']
+        @list.items = ['apple']
         #assert('apple' == list.join, "Выражение не соответствует 'apple'") 
         #Вывели расшифровку ошибки при тестировании
-        assert_equal('apple', list.join)
+        assert_equal('apple', @list.join)
         #Вывели значение которое ждали и которое получили
     end
 
     def test_it_joins_two_words_with_and
-        list = ListWithCommas.new
-        list.items = ['apple', 'orange']
-        assert_equal('apple and orange', list.join)
+        @list.items = ['apple', 'orange']
+        assert_equal('apple and orange', @list.join)
     end
 
     def test_it_joins_three_words_with_commas
-        list = ListWithCommas.new
-        list.items = ['apple', 'orange', 'pear']
-        assert_equal('apple, orange, and pear', list.join)
+        @list.items = ['apple', 'orange', 'pear']
+        assert_equal('apple, orange, and pear', @list.join)
     end
 end
