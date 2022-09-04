8ca53c8fd57b041d619ef3be8d53d9caba6d95c6
Cometa09 <sokolovalex09@gmail.com>
2022-09-04 18:01:39 +0300

    add Module condition

---
+++ 13 tests/test/test_list_with_commas.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'list_with_commas'
+
+class TestListWithCommas < Minitest::Test
+
+    def test_it_prints_one_word_alone
+        list = ListWithCommas.new
+        list.items = ['apple']
+        #assert('apple' == list.join, "Выражение не соответствует 'apple'") 
+        #Вывели расшифровку ошибки при тестировании
+        assert_equal('apple', list.join)
+        #Вывели значение которое ждали и которое получили
+    end
+
+    def test_it_joins_two_words_with_and
+        list = ListWithCommas.new
+        list.items = ['apple', 'orange']
+        assert_equal('apple and orange', list.join)
+    end
+
+    def test_it_joins_three_words_with_commas
+        list = ListWithCommas.new
+        list.items = ['apple', 'orange', 'pear']
+        assert_equal('apple, orange, and pear', list.join)
+    end
+end
